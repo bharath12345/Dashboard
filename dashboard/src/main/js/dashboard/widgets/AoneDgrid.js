1ad15d921870ed2a0d0d4f2f61a4b70686011780
bharadwaj <bharath12345@gmail.com>
2013-03-16 00:53:04 +0530

    Dgrid issues
    dont make dgrid to override _WidgetBase - it screws up the rendering

--- dashboard/src/main/js/dashboard/widgets/AoneDgrid.js
+++ dashboard/src/main/js/dashboard/widgets/AoneDgrid.js
@@ -1,13 +1,12 @@
 define(["dojo/_base/declare", "dojo/i18n", "dojo/i18n!dashboard/nls/dashboard", "dashboard/logger/Logger",
-    'dgrid/Grid', 'dijit/_WidgetBase', "dgrid/extensions/ColumnHider", "dgrid/extensions/ColumnReorder", "dgrid/extensions/ColumnResizer",
+    'dgrid/Grid', "dgrid/extensions/ColumnHider", "dgrid/extensions/ColumnReorder", "dgrid/extensions/ColumnResizer",
     "dgrid/extensions/DijitRegistry", "dgrid/extensions/Pagination", "dgrid/Selection"],
 
-    function (declare, i18n, i18nString, Logger, Grid, _WidgetBase, ColumnHider, ColumnReorder, ColumnResizer,
-              DijitRegistry, Pagination, Selection) {
+    function (declare, i18n, i18nString, Logger, Grid, ColumnHider, ColumnReorder, ColumnResizer, DijitRegistry, Pagination, Selection) {
 
         dashboard.classnames.AoneDgrid = "dashboard.widgets.AoneDgrid";
 
-        var AoneDgrid = declare(dashboard.classnames.AoneDgrid, [_WidgetBase, Grid, ColumnHider, ColumnReorder, ColumnResizer, DijitRegistry, Pagination, Selection], {
+        var AoneDgrid = declare(dashboard.classnames.AoneDgrid, [Grid, ColumnHider, ColumnReorder, ColumnResizer, DijitRegistry, Pagination, Selection], {
 
             // the Grid + ColumnHider Mixin is needed for the 'id' column to be hidden
 
