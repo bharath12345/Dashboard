8b447c39b1ca7c1521d0781307c523f079e8472b
bharadwaj <bharath12345@gmail.com>
2013-04-15 13:25:57 +0530

    removed xml core - xml beans takes care of this now

--- uiExtensionLib/src/main/java/com/appnomic/appsone/ui/extension/xml/core/A1UIFormToolbar.java
+++
@@ -1,9 +0,0 @@
-package com.appnomic.appsone.ui.extension.xml.core;
-
-/**
- * User: bharadwaj
- * Date: 09/04/13
- * Time: 1:00 PM
- */
-public class A1UIFormToolbar {
-}
--- uiExtensionLib/src/main/java/com/appnomic/appsone/ui/extension/xml/core/A1UIPaneMenu.java
+++
@@ -1,9 +0,0 @@
-package com.appnomic.appsone.ui.extension.xml.core;
-
-/**
- * User: bharadwaj
- * Date: 09/04/13
- * Time: 12:59 PM
- */
-public class A1UIPaneMenu {
-}
